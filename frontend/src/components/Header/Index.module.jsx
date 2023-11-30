2384bf430f84e6374da4d83b62473e80173e9526
Caioã Rodrigues <caioarodrigues@alu.ufc.br>
2023-11-30 17:18:00 -0300

    adicionando um pouco de css

--- frontend/src/components/Header/Index.module.jsx
+++ frontend/src/components/Header/Index.module.jsx
@@ -1,7 +1,9 @@
 /* eslint-disable react/prop-types */
+import "./style.css"
+
 export default function Header ({ titulo, descricao }) {
     return (
-        <header>
+        <header className="header">
             <h2>{titulo}</h2>
             {descricao ? <h3> {descricao} </h3> : <></>}
         </header>
--- frontend/src/routes/Home/Index.module.jsx
+++ frontend/src/routes/Home/Index.module.jsx
@@ -4,22 +4,25 @@ import "./style.css";
 export default function Home () {
     return (
         <>
-            <Header titulo={"Controle do sistema de irrigação e iluminação"}
-                descricao={"Trabalho de STR"}/>
-            <ul>
-                <li>
-                    <a href="/lista">Visualizar todas as plantas</a>
-                </li>
-                <li>
-                    <a href="/adiciona-planta">Adicionar uma nova planta</a>
-                </li>
-                <li>
-                    <a href="/remove-planta">Remover uma planta</a>
-                </li>
-                <li>
-                    <a href="/cria-registro">Registrar status de uma planta</a>
-                </li>
-            </ul>
+            <Header titulo={"Controle do sistema de irrigação e iluminação"} descricao=
+                {"Trabalho de STR"}/>
+            <body className="body">
+                <h3>Selecione uma opção</h3>
+                <ul className="lista-opcoes">
+                    <li>
+                        <a href="/lista">Visualizar todas as plantas</a>
+                    </li>
+                    <li>
+                        <a href="/adiciona-planta">Adicionar uma nova planta</a>
+                    </li>
+                    <li>
+                        <a href="/remove-planta">Remover uma planta</a>
+                    </li>
+                    <li>
+                        <a href="/cria-registro">Registrar status de uma planta</a>
+                    </li>
+                </ul>
+            </body>
         </>
     )
 }
